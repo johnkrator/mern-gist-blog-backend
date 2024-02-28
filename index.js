a37d57c13aee39ffbe23131ccd2bc49e155560bd
johncreator <chukwuchidieberejohn@gmail.com>
2024-02-28 02:26:16 +0100

    Implement user auth logic

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -14,3 +17,5 @@ mongoose
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
+app.use("/api/auth", authRoute);
